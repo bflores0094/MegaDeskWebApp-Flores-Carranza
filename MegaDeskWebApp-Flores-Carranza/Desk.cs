d21cc7a3315dcb8381cdbe085d7b6e718f7d956c
bflores0094 <bflores0094@gmail.com>
2018-11-17 22:01:17 -0700

    Add, delete and edit working

---
+++ MegaDeskWebApp-Flores-Carranza/Desk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace MegaDeskWebApp_Flores_Carranza
+
+{
+    public struct Desk
+    {
+        public decimal widthInput { get; set; }
+        public decimal heightInput { get; set; }
+        public decimal drawerInput { get; set; }
+        public decimal materialNum { get; set; }
+
+        public Desk(decimal widthInput, decimal heightInput, decimal drawerInput, decimal materialNum)
+        {
+
+            this.widthInput = widthInput;
+            this.heightInput = heightInput;
+            this.drawerInput = drawerInput;
+            this.materialNum = materialNum;
+
+        }
+    }
+}
